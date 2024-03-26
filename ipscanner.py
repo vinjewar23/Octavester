1253c09d9b86e05e9a8cfc9e211aa45ae058c1a0
Phoenixcybersec <95076195+vinjewar23@users.noreply.github.com>
2024-03-26 16:02:30 +0530

    Update ipscanner.py

--- ipscanner.py
+++ ipscanner.py
@@ -1,12 +1,11 @@
-import os 
 import sys
+import nmap 
 
 ip = input("\033[31mEnter the IP address: \033[31m")
 
 while(True):
     print("\033[1;91m1. Port Scanning")
     print("2. Version Detection Scan")
-    print("3. Vulnerability Scan")
     print("99. Exit\033[1;m")
 
     option = int(input("\033[36m\nEnter your option: \033[1;m"))
@@ -18,29 +17,54 @@ while(True):
     █                              Port Scanning                                  █ 
     █                                                                             █
     └═════════════════════════════════════════════════════════════════════════════┘     \n \033[1;m""")
-        command = os.system("nmap "+ip)
+        # initialize the port scanner
+        nmScan = nmap.PortScanner()
 
+        # scan for ports in range 21-443
+        nmScan.scan(ip)
+
+        # run a loop to print all the found result about the ports
+        for host in nmScan.all_hosts():
+            print('\033[35mHost : %s ' % (host))
+            print('State : %s' % nmScan[host].state())
+            for proto in nmScan[host].all_protocols():
+                print('----------')
+                print('Protocol : %s' % proto)
+ 
+                lport = nmScan[host][proto].keys()
+                lport=sorted(lport)
+            for port in lport:
+                print ('port : %s\tstate : %s ' % (port, nmScan[host][proto][port]['state']))
+            print('\n \033[1;m')
 
 
     elif(option == 2):
-        print(""" \033[1;36m
+        print(""" \033[1;33m
     ┌═════════════════════════════════════════════════════════════════════════════┐
     █                                                                             █
     █                           Version Detection Scan                            █ 
     █                                                                             █
     └═════════════════════════════════════════════════════════════════════════════┘     \n \033[1;m""")
-        command = os.system("nmap -sV "+ ip)
+        # initialize the port scanner
+        nmScan = nmap.PortScanner()
 
+        # scan for ports in range 21-443
+        nmScan.scan(ip, arguments='-sV')
+
+        # run a loop to print all the found result about the ports
+        for host in nmScan.all_hosts():
+            print('\033[35mHost : %s ' % (host))
+            print('State : %s' % nmScan[host].state())
+            for proto in nmScan[host].all_protocols():
+                print('----------')
+                print('Protocol : %s' % proto)
+ 
+                lport = nmScan[host][proto].keys()
+                lport=sorted(lport)
+            for port in lport:
+                print ('port : %s\tstate : %s\tService : %s\tVersion : %s ' % (port, nmScan[host][proto][port]['state'],nmScan[host][proto][port]['name'],nmScan[host][proto][port]['product']))
+            print('\n \033[1;m')
         
-
-    elif(option == 3):
-        print(""" \033[1;36m
-    ┌═════════════════════════════════════════════════════════════════════════════┐
-    █                                                                             █
-    █                             Vulnerability Scan                              █ 
-    █                                                                             █
-    └═════════════════════════════════════════════════════════════════════════════┘     \n \033[1;m""")
-        command = os.system("nmap -sV -sC "+ ip)
 
     elif(option == 99):
         input("\n\033[33mPress Enter to exit\n\n\033[0m")
