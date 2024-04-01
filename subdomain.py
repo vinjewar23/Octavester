fffd5d59840e4eccbf584f00010b99db05a81bdc
Phoenixcybersec <95076195+vinjewar23@users.noreply.github.com>
2024-04-01 14:34:14 +0530

    Update subdomain.py

--- subdomain.py
+++ subdomain.py
@@ -2,9 +2,9 @@ import requests
  
 # function for scanning subdomains
 def domain_scanner(domain_name,sub_domain_names):
-    print('-----------Scanner Started-----------')
+    print('\033[1;36m-----------Scanner Started-----------')
     print('----------Subdomains found:----------')
-
+     
     for subdomain in sub_domain_names:
        
         # making url by putting subdomain one by one
@@ -12,8 +12,10 @@ def domain_scanner(domain_name,sub_domain_names):
          
         # using try catch block to avoid crash of
         # the program
-        try:  
-            requests.get(url) 
+        try:
+            requests.get(url)
+             
+            # if after putting subdomain one by one url 
             # is valid then printing the url
             print(f'[+] {url}')
              
@@ -27,18 +29,16 @@ def domain_scanner(domain_name,sub_domain_names):
 # main function
 if __name__ == '__main__':
    
-    # inputting the domain name
-    domain_name = input("Enter the Domain Name:")
+    domain_name = input("\033[1;31mEnter the Domain Name: ")
     print('\n')
  
     # opening the subdomain text file
     with open('subdomains.txt','r') as file:
-       
-        # reading the file
+
         name = file.read()
          
         # using splitlines() function storing the 
         # list of splitted strings
         sub_domain = name.splitlines()
-         
+        
     domain_scanner(domain_name,sub_domain)
